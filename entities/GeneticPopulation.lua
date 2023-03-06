bb4fb10afe1a0b9ce0e44dbd286c9efa559f6227
passolargo11 <guiprada@gmail.com>
2023-03-06 10:33:39 -0300

    - Fixed mule start.

--- entities/GeneticPopulation.lua
+++ entities/GeneticPopulation.lua
@@ -202,7 +202,7 @@ function GeneticPopulation:replace(i)
 			if self._specie_mule_start then
 				if self._specie_niche_count[specie_id] > (self._specie_initial_population_size/3) then
 					mom = specie:roulette()
-					local specie_index = self.specie_niche_count[specie_id]%(#self._species)
+					local specie_index = self._specie_niche_count[specie_id]%(#self._species)
 					print("mule crossing with: ", specie_index)
 					local dad_species = self._species[specie_index]
 						dad = dad_species:_roulette(self._history)
